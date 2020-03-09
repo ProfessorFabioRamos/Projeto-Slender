6904a77e802b74c910154a82f5b35601222e8d5a
Prof Fabio <fabio.sramos@ceub.edu.br>
2020-03-09 19:29:31 -0300

    09_03_20

---
+++ Slender/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flashlight : MonoBehaviour {
+    public Light luz;
+    public AudioSource aud;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            luz.enabled = !luz.enabled;
+            aud.Play();
+        }
+	}
+}
