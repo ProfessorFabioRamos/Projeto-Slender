b8e877eadfb9737dd4ef0add0167908f6439ba8c
Professor Fabio Ramos <fabio.sramos@ceub.edu.br>
2020-03-25 22:50:20 -0300

    25-03

---
+++ Slender/Assets/Scripts/Collect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collect : MonoBehaviour
+{
+    public int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.tag == "Collectible"){
+            //score = score +1;
+            //score += 1;
+            score++;
+            Destroy(other.gameObject);
+        }
+    }
+}
