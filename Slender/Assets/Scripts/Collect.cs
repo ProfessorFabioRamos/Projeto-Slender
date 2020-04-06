e530fb15899d2ccab05713c2baeedb2a3e3e1df2
Professor Fabio Ramos <fabio.sramos@ceub.edu.br>
2020-04-06 20:29:53 -0300

    06-04-20_2

--- Slender/Assets/Scripts/Collect.cs
+++ Slender/Assets/Scripts/Collect.cs
@@ -8,11 +8,18 @@ public class Collect : MonoBehaviour
     public int score = 0;
     public bool completed = false;
     public Text scoreText;
+    public GameObject hintTextPanel;
+    [SerializeField]
+    private GameObject gun;
+    private AudioSource aud;
+    public AudioClip reloadClip;
 
     // Start is called before the first frame update
     void Start()
     {
         scoreText.text = score+"/8 Caveiras";
+        gun.SetActive(false);
+        aud = GetComponent<AudioSource>();
     }
 
     void OnTriggerEnter(Collider other){
@@ -24,6 +31,27 @@ public class Collect : MonoBehaviour
             Destroy(other.gameObject);
             if(score >= 8)
                 completed = true;
+        }
+    }
+
+    void OnTriggerStay(Collider other){
+        if(other.gameObject.tag == "Gun"){
+            hintTextPanel.SetActive(true);
+            hintTextPanel.transform.GetComponentInChildren<Text>().text = "Pressione E para pegar";
+            //if(Input.GetButtonDown("Interact"))
+            if(Input.GetKeyDown(KeyCode.E)){
+                Destroy(other.gameObject);
+                hintTextPanel.SetActive(false);
+                gun.SetActive(true);
+                aud.clip = reloadClip;
+                aud.Play();
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other){
+        if(other.gameObject.tag == "Gun"){
+            hintTextPanel.SetActive(false);
         }
     }
 }
